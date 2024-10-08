d4cf2a0d67ad2a80466d223c6d8ee3b15c1988a0
yijoon009 <yijoon009@gmail.com>
2024-10-08 14:47:19 +0900

    refactor: app.py 분리

--- dags/model_pipeline_dag.py
+++ dags/model_pipeline_dag.py
@@ -7,11 +7,6 @@ import time
 def train_model():
     subprocess.run(['python', '/opt/airflow/scripts/model.py'])
 
-def serve_model():
-    process = subprocess.Popen(['python', '/opt/airflow/scripts/app.py'])
-    time.sleep(10)  # Wait for the FastAPI app to start
-    return process.pid
-
 def fetch_new_data():
     print("Fetching new data... (currently a placeholder)")
 
@@ -35,14 +30,9 @@ with DAG(
         python_callable=train_model,
     )
 
-    t2 = PythonOperator(
-        task_id='serve_model',
-        python_callable=serve_model,
-    )
-
     t3 = PythonOperator(
         task_id='fetch_new_data',
         python_callable=fetch_new_data,
     )
 
-    t1 >> t2 >> t3
+    t1 >> t3
