4f1f4968b68586971b7f5e73f2dd02a980e2cb9d
chosungsu <ski06043@naver.com>
2024-10-08 15:49:49 +0900

    commit: adding model code

---
+++ scripts/preprocess.py
@@ -0,0 +1,14 @@
+import mlflow
+from sklearn.preprocessing import StandardScaler
+
+features = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms']
+
+# 데이터 전처리 및 스케일링 함수
+def preprocess(data):
+    X = data[features]
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+    mlflow.log_param("scaler", scaler)
+    mlflow.log_param("features", features)
+
+    return X_scaled, scaler
