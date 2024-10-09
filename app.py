817bd8c4434380024ab0310d34ce02cac5849243
Sejin Kwon <sjkwon1023@gmail.com>
2024-10-10 02:14:26 +0900

    sejin

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
-from fastapi import FastAPI, HTTPException
-from dags.scripts.SongInput import SongInput
+from fastapi import FastAPI
+# from dags.scripts.load_model import load_or_train_model
 from dags.scripts.model import KMeans
-from dags.scripts.load_model import load_model
+import pandas as pd
+import sklearn
+import pickle
 
 app = FastAPI()
 
 # 글로벌 모델과 스케일러 로드
-kmeans_model, data = load_model()
+kmeans_model_path = "/app/dags/model/kmeans_model.pkl"
+knn_model_path = "/app/dags/model/knn_model.pkl"
+train_data_path = "/app/dags/data/train.csv"
 
-@app.post("/api/v1/recommend_songs")
-def recommend_songs(input: SongInput):
-    song_name = input.song_name
+with open(knn_model_path, 'rb') as file:
+    knn_model = pickle.load(file)
+data = pd.read_csv(train_data_path)
 
-    # KMeans 추천 시스템 인스턴스 생성
-    recommender = KMeans(song_name, kmeans_model, data)
-
-    try:
-        # 추천된 노래 목록 반환
-        recommendations = recommender.recommend_songs_by_knn(top_n=10)
-        return recommendations.to_dict(orient='records')
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error in recommendation: {str(e)}")
+@app.post("/recommend")
+def recommend_songs(song_name: str):
+    if not song_name.strip():
+        return {"error": "Song name cannot be empty."}
+    
+    # 추천 실행
+    recommend = KMeans(song_name, knn_model, data)
+    recommended_songs = recommend.recommend_songs_by_knn()
+    
+    # recommended_songs가 dict일 경우 바로 반환
+    if isinstance(recommended_songs, dict):
+        return recommended_songs
+    
+    # recommended_songs가 DataFrame일 경우 to_dict 호출
+    return {"recommendations": recommended_songs.to_dict(orient="records")}
--- dags/model_pipeline_dag.py
+++ dags/model_pipeline_dag.py
@@ -1,15 +1,34 @@
+# import sys
+# import os
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime
-from scripts.load_model import load_or_train_model
+# 현재 파일의 경로를 기준으로 scripts 폴더 추가
+# sys.path.append(os.path.join(os.path.dirname(__file__), 'scripts'))
+
+from scripts.load_model import load_model
+from scripts.spotify_api import Spotify_Weekly_Chart, Get_Info
 
 
 def train_model():
-    # 여기에 train_model.py 함수사용해야함.
-    pass
+    # 모델 훈련 및 MLflow 로깅
+    load_model()
     
 def fetch_new_data():
-    print("Fetching new data... (currently a placeholder)")
+    try:
+        countries = ['kr', 'us', 'global']
+        downloader = Spotify_Weekly_Chart(countries)
+        downloader.download_charts('sejin_kwon@naver.com', 'qykfab-5reZqu-pafhug')
+
+        spotify_api = Get_Info()
+        spotify_api.fetch()
+    
+    except Exception as e:
+        print(f"Error in fetch_new_data: {e}, Probably no new songs updated in the chart")
+        raise
+
+
 
 default_args = {
     'owner': 'airflow',
@@ -22,19 +41,22 @@ with DAG(
     'model_pipeline',
     default_args=default_args,
     description='A simple model training and serving DAG',
-    schedule_interval='@daily',
+    schedule_interval='@weekly',
     catchup=False,
 ) as dag:
 
     t1 = PythonOperator(
-        task_id='fetch_new_data',
-        python_callable=fetch_new_data,
-    )
-    
-    t2 = PythonOperator(
         task_id='train_model',
         python_callable=train_model,
     )
 
+    t2 = DockerOperator(
+        task_id='fetch_spotify_data',
+        image='mlops-spotify_api',  # 빌드된 도커 이미지 이름
+        auto_remove=True,
+        command="python /app/dags/script/spotify_api.py",  # 스크립트를 실행하는 명령어
+        docker_url='unix://var/run/docker.sock',
+        network_mode='bridge'
+    )
 
-t1 >> t2
+t2 >> t1
